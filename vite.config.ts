8ebbe0eeabea93b6753e2d3df6e7b980128371f6
Shakij Mahamud <shakijx@gmail.com>
2025-12-02 04:44:23 +0600

    added files

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
 
-      base: '/',
+      base: '/bba-tc/',
 
       server: {
         port: 3000,
